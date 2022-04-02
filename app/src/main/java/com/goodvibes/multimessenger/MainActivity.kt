19a19380a24cce0f63eecabeb95c737485b3845e
Lyalyashechka <Rudukh.Eugene@yandex.ru>
2022-04-03 02:02:04 +0300

    action mode in chat item

--- app/src/main/java/com/goodvibes/multimessenger/MainActivity.kt
+++ app/src/main/java/com/goodvibes/multimessenger/MainActivity.kt
@@ -3,10 +3,12 @@ package com.goodvibes.multimessenger
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 
@@ -16,10 +18,13 @@ import com.goodvibes.multimessenger.datastructure.Chat
 import com.goodvibes.multimessenger.datastructure.Messengers
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
     lateinit var activityMainBinding : ActivityMainBinding;
     lateinit var toggle : ActionBarDrawerToggle
     lateinit var toolbar: Toolbar
+    var mActionMode: ActionMode? = null
+    lateinit var callback: ListChatsActionModeCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
 
@@ -27,21 +32,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         initMenu()
+        initChatsAllAdapter()
 
-
-
-        var img = arrayOf<Int>(R.drawable.kotik, R.drawable.kotik, R.drawable.kotik);
-        var title = arrayListOf<String>("Sergey Kust1", "Sergey Kust2", "Sergey Kust3");
-        var lastMessage = arrayListOf<String>("Hello", "how are you", "fsdgdf");
-        var messangerType = arrayListOf<Messengers>(Messengers.VK, Messengers.TELEGRAM, Messengers.TELEGRAM);
-
-        var chats = arrayListOf<Chat>();
-        for(i in 0..2){
-            chats += Chat(img[i], title[i], lastMessage[i], messangerType[i]);
-        }
-
-        var listChatsAdapter: ListChatsAdapter = ListChatsAdapter(this, chats);
-        activityMainBinding.listChats.setAdapter(listChatsAdapter);
+        callback = ListChatsActionModeCallback()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -57,7 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
@@ -71,4 +64,58 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
+
+    private fun initChatsAllAdapter() {
+        var img = arrayOf<Int>(R.drawable.kotik, R.drawable.kotik, R.drawable.kotik);
+        var title = arrayListOf<String>("Sergey Kust1", "Sergey Kust2", "Sergey Kust3");
+        var lastMessage = arrayListOf<String>("Hello", "how are you", "fsdgdf");
+        var messangerType = arrayListOf<Messengers>(Messengers.VK, Messengers.TELEGRAM, Messengers.TELEGRAM);
+
+        var chats = arrayListOf<Chat>();
+        for(i in 0..2){
+            chats += Chat(img[i], title[i], lastMessage[i], messangerType[i]);
+        }
+
+        var listChatsAdapter: ListChatsAdapter = ListChatsAdapter(this, chats);
+        activityMainBinding.listChats.setAdapter(listChatsAdapter);
+        activityMainBinding.listChats.setOnItemLongClickListener { parent, view, position, id ->
+            if (mActionMode != null) {
+                false
+            }
+            mActionMode = startSupportActionMode(callback)!!
+            true
+        }
+    }
+
+
+    inner class ListChatsActionModeCallback : ActionMode.Callback {
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            mode?.menuInflater?.inflate(R.menu.select_chat_menu, menu)
+            mode?.setTitle("choose your option")
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            return false
+        }
+
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            when (item?.itemId) {
+                R.id.select_chat_menu_delete -> {
+                    mode?.finish()
+                    return true
+                }
+            }
+            return false;
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            mActionMode = null;
+        }
+    }
+
 }
+
+
+
